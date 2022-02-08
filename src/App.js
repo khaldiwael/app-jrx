f175c57262c42cf215ba5b16a84968994d7983a0
Your Name <you@example.com>
2022-02-08 22:25:37 +0100

    last commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import imgsrc from "./imgsrc.jpg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+  
+      <div className='main' style={{border:'solid 1px black',maxWidth:'100vw'}}>
+        <h1 class="title red">wael khaldi</h1>
+        <br/>
+        <img src={imgsrc} className='profile'/>
+        <br/>
+        <img src="./img public.jpg"/>
+      </div>
+
+       <video width="320" height="240" controls>
+        <source src="myVideo.mp4" type="video/mp4" />
+      </video>
+  
+    </>
   );
 }
 
